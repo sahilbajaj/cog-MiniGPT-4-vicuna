252a4a7048f4a26fd43ee4dd7447934b26f4e444
Nelson Chen <crazysim@gmail.com>
2023-04-23 16:54:41 +0000

    First successful caption!

--- predict.py
+++ predict.py
@@ -1,3 +1,9 @@
+import argparse
+from PIL import Image
+from minigpt4.conversation.conversation import CONV_VISION, Chat
+from minigpt4.common.registry import registry
+from minigpt4.common.config import Config
+from cog import BasePredictor, Input, Path
 import os
 cache = "/src/weights/"
 os.environ["TORCH_HOME"] = "/src/weights/"
@@ -6,19 +12,15 @@ os.environ["HUGGINGFACE_HUB_CACHE"] = "/src/weights/"
 if not os.path.exists(cache):
     os.makedirs(cache)
 
-from cog import BasePredictor, Input
-from minigpt4.common.config import Config
-from minigpt4.common.registry import registry
-from minigpt4.conversation.conversation import Chat
-
-import argparse
-import os
 
 class Predictor(BasePredictor):
     def setup(self):
+        # Copied as-is from demo.py
         parser = argparse.ArgumentParser(description="Demo")
-        parser.add_argument("--cfg-path", required=True, help="path to configuration file.")
-        parser.add_argument("--gpu-id", type=int, default=0, help="specify the gpu to load the model.")
+        parser.add_argument("--cfg-path", required=True,
+                            help="path to configuration file.")
+        parser.add_argument("--gpu-id", type=int, default=0,
+                            help="specify the gpu to load the model.")
         parser.add_argument(
             "--options",
             nargs="+",
@@ -26,20 +28,49 @@ class Predictor(BasePredictor):
             "in xxx=yyy format will be merged into config file (deprecate), "
             "change to --cfg-options instead.",
         )
-        args = parser.parse_args(["--cfg-path","eval_configs/minigpt4_eval.yaml"])
+        args = parser.parse_args(
+            ["--cfg-path", "eval_configs/minigpt4_eval.yaml"])
 
         cfg = Config(args)
 
         model_config = cfg.model_cfg
         model_config.device_8bit = args.gpu_id
         model_cls = registry.get_model_class(model_config.arch)
-        self.model = model_cls.from_config(model_config).to('cuda:{}'.format(args.gpu_id))
+        self.model = model_cls.from_config(
+            model_config).to('cuda:{}'.format(args.gpu_id))
 
         vis_processor_cfg = cfg.datasets_cfg.cc_sbu_align.vis_processor.train
-        self.vis_processor = registry.get_processor_class(vis_processor_cfg.name).from_config(vis_processor_cfg)
-        self.chat = Chat(self.model, self.vis_processor, device='cuda:{}'.format(args.gpu_id))
+        self.vis_processor = registry.get_processor_class(
+            vis_processor_cfg.name).from_config(vis_processor_cfg)
+        self.chat = Chat(self.model, self.vis_processor,
+                         device='cuda:{}'.format(args.gpu_id))
 
         self.prefix = "hello"
 
-    def predict(self, text: str = Input(description="Text to prefix with 'hello '")) -> str:
-        return self.prefix + " " + text
+    def predict(
+        self,
+        image: Path = Input(
+            description="Input image to discuss"),
+        message: str = Input(
+            description="Message to send to the bot.",
+            default="Please describe the image.",
+        ),
+    ) -> str:
+        """Run a single prediction on the model"""
+        chat = self.chat
+        raw_image = Image.open(image).convert("RGB")
+        chat_state = CONV_VISION.copy()
+        img_list = []
+        llm_message = chat.upload_img(raw_image, chat_state, img_list)
+        num_beams, temperature = 1, 1.0
+        chat.ask(message, chat_state)
+
+        llm_message = chat.answer(
+            conv=chat_state,
+            img_list=img_list,
+            max_new_tokens=1000,
+            num_beams=num_beams,
+            temperature=temperature
+        )[0]
+
+        return llm_message
